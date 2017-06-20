927799ffde36fe225a62fd29e55666fc661254db
greatjing <greatjing@sina.cn>
2017-06-20 13:22:39 +0800

    add account show info

--- app/controllers/test_reports_controller.rb
+++ app/controllers/test_reports_controller.rb
@@ -1,7 +1,12 @@
 class TestReportsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
   def index
     @test_reports = TestReport.all
+  end
+
+  def show
+    @test_report = TestReport.find(params[:id])
   end
 
 end
