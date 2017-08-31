7f73399b343bac9babc5c877b835e1198684f586
greatjing <greatjing@sina.cn>
2017-08-31 23:22:18 +0800

    part front and back_end js css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,7 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
-//= require select2
-//= require nested_form_fields
+
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require_tree .
